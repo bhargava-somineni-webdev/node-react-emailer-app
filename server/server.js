6fa166d2abd3c0bd142112d5194ceb3851db6913
bhargava-somineni <somineni3@gmail.com>
2017-07-28 20:24:44 -0400

    Minor change to server

--- server/server.js
+++ server/server.js
@@ -3,6 +3,10 @@ const port = process.env.PORT || 3000;
 
 var app = express();
 
+app.get('/', (req, res)=>{
+res.send('Life is pretty.');
+});
+
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
