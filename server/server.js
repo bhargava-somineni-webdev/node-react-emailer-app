c36e7dd20368e5c2cef88a8bcd9e950f9eb2d7f2
bhargava-somineni <somineni3@gmail.com>
2017-07-30 18:56:30 -0400

    Correction to client build folder

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
@@ -39,16 +40,16 @@ billingRoutes(app);
 
 if (process.env.NODE_ENV === 'production') {
     //Express will serve up production assests like main.js or main.css files
-    //below code says that,if some get request comes for any resources(files)
-    // we do not understand then look into client/build 
+    //below code says that,if some get request comes for any resource(files)
+    //that we do not understand then look into client/build 
     //to see for that file and load that file
     //Eg: client/build/static/css/main.css
     app.use(express.static('client/build'));
 
     //Express will serve up index.html file if it doesnt  recognize a route
     const path = require('path');
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    app.get('*', (req, res) => {       
+        res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
     });
 }
 
